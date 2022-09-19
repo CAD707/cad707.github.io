ce228aae6d12d4afacf0f61aaa8dcc7ed2f5679a
CAD707 <111105406+CAD707@users.noreply.github.com>
2022-09-19 10:58:11 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,17 +1,11 @@
-<?php
-   $imagesDir = 'cards/';
-   $images = glob($imagesDir . '*.{jpg,jpeg,png,gif}', GLOB_BRACE);
-   $randomImage = $images[array_rand($images)];
-   echo "<img src='$randomImage'>";
-?>
+<!-- include aframe obviously -->
 <script src="https://aframe.io/releases/0.6.0/aframe.min.js"></script>
-
+<!-- include ar.js for a-frame -->
 <script src="https://jeromeetienne.github.io/AR.js/aframe/build/aframe-ar.js"></script>
 <body style='margin : 0px; overflow: hidden;'>
 	<a-scene embedded arjs>
 		<!-- create your content here. just a box for now -->
-		<a-image position='0 0.5 0' material='opacity: 0.5;'src=<?php '$randomImage'?>></a-image>
-		
+		<a-box position='0 0.5 0' material='opacity: 0.5;'></a-box>
 		<!-- define a camera which will move according to the marker position -->
 		<a-marker-camera preset='hiro'></a-marker-camera>
 	</a-scene>
